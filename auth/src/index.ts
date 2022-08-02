e28504176cedd96bfd4f772c7768840775383066
HunterX18 <srsahu13@gmail.com>
2022-08-02 12:27:27 +0530

    created user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import 'express-async-errors';
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -17,11 +18,21 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all("*", async () => {
-    throw new NotFoundError();
+	throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log("Listening on port 3000!");
-});
+const start = async () => {
+	try {
+		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+		console.log("Connected to MongoDB");
+	} catch (err) {
+		console.log(err);
+	}
+	app.listen(3000, () => {
+		console.log("Listening on port 3000!");
+	});
+};
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from '../models/user'
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 
 router.post(
@@ -19,9 +20,13 @@ router.post(
             throw new RequestValidationError(errors.array());
 		}
 		const { email, password } = req.body;
-		console.log("Creating a user...");
-        throw new DatabaseConnectionError();
-		res.send({});
+		const existingUser = await User.findOne({ email });
+		if(existingUser) {
+			throw new BadRequestError("Email in use")
+		}
+		const user = User.build({ email, password });
+		await user.save();
+		res.status(201).send(user);
 	}
 );
 
