b069e56ad5a3b070f42189fc19b0a53e007692ae
HunterX18 <srsahu13@gmail.com>
2022-08-23 12:23:16 +0530

    auth setup completed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import buildClient from "../api/build-client";
 
-const index = () => {
-  return (
-    <div>Landing Page</div>
-  )
-}
+const LandingPage = ({ currentUser }) => {
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are not signed in</h1>
+	);
+};
 
-export default index
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+	const { data } = await client
+		.get("/api/users/currentuser")
+		.catch((err) => console.log(err.message));
+	return data;
+};
+
+export default LandingPage;
