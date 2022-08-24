463f2ff969eecf15d6293bb93923b2f0b4cc3640
HunterX18 <srsahu13@gmail.com>
2022-08-24 14:08:05 +0530

    custom package created

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -6,8 +6,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler, NotFoundError } from "@srstickets/common";
 
 const app = express();
 app.set("trust proxy", true);
@@ -15,7 +14,7 @@ app.use(json());
 app.use(
 	cookieSession({
 		signed: false,
-		secure: process.env.NODE_ENV !== 'test',
+		secure: process.env.NODE_ENV !== "test",
 	})
 );
 app.use(currentUserRouter);
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@srstickets/common";
 import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
 
